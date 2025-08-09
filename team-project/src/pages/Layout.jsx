b2b8ee2b79e2054cae5e3a7093af5bfa31336f0a
Daler <rahmatulloevdaler@mail.ru>
2025-08-09 11:33:06 +0500

    настройка

---
+++ team-project/src/pages/Layout.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom"
+
+export const Layout = () => {
+    return (
+        <div className="">
+            <nav>
+                <div className="">
+                    <Link to={"/"}>Home</Link>
+                    <Link to={"/about"}>About</Link>
+                    <Link to={"/pricing"}>Pricing</Link>
+                </div>
+            </nav>
+
+            <Outlet />
+        </div>
+    )
+}
